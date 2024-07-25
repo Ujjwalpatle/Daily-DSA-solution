130c85f4b4d48988e4739ff782f91d9549366ed5
Ujjwal patle <136487317+Ujjwalpatle@users.noreply.github.com>
2024-07-25 19:43:28 +0530

    Add files via upload

---
+++ Product of Array Except Self.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        ans[0] = 1;
+        for(int i = 1; i < n; i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        int sum=1;
+        for(int j = n - 1;j >= 0; j--){
+            ans[j] *= sum;
+            sum *= nums[j];
+        }
+        return ans;
+    }
+}
+
